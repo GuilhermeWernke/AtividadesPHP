9ce4b6ba9a9a8eefcf7ab65a49c8b10f9db2aff9
GuilhermeWernke <134728943+GuilhermeWernke@users.noreply.github.com>
2023-11-22 17:44:24 -0300

    Pokémon.php

---
+++ Pokémon.php
@@ -0,0 +1,32 @@
+<?php
+     
+    $dados_em_texto;
+    $pos_ataque = 1;
+    $qual_pokemon;
+
+    print("Escolha um pokemon usando somente letras minusculas! \n");
+    $qual_pokemon = readline("\n");
+    $dados_em_texto = file_get_contents("https://pokeapi.co/api/v2/pokemon/$qual_pokemon");
+    
+
+
+    $pokemon = json_decode($dados_em_texto, true);
+
+    print("Nome: ");
+    print_r($pokemon['name']);
+    print("\n");
+
+    print("Altura: ");
+    print_r($pokemon['height'] / 10);
+    print("\n");
+
+    print("Peso: ");
+    print_r($pokemon['weight']);
+    print("\n");
+
+    foreach($pokemon['moves'] as $move)
+    {
+        print("Ataque $pos_ataque: " . $move['move']['name'] . "\n");
+        $pos_ataque++;
+    }
+?>
