07502694d0379f2029a8f591403e4e73c5af0638
GuilhermeWernke <134728943+GuilhermeWernke@users.noreply.github.com>
2023-11-29 17:53:51 -0300

    Pokémon.php

--- Pokémon.php
+++ Pokémon.php
@@ -30,3 +30,78 @@
         $pos_ataque++;
     }
 ?>
+
+<?php
+
+    $pokeapi = file_get_contents("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+    $pokemons = json_decode($pokeapi, true);
+
+    for($i = 0; $i < 20; $i++)
+    {
+        $pokemon = $pokemons['results'][$i];
+
+        $todas_info_api = file_get_contents($pokemon['url']);
+        $pokemon['results'][$i] = json_decode($todas_info_api, true);
+    }
+    
+?>
+<html>
+<html lang="en">
+
+    <head>
+        <title>Pokedex</title>
+
+        <style>
+
+            #pesquisa
+            {
+                background: red;
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+                padding: 20px;
+                text-alling: center;
+
+            }
+
+            .pokemon
+            {
+                width: 20%;
+                border: solid 2px #555;
+                padding: 15px;
+                margin: 20px;
+                text-align: center;
+                float: left;
+            }
+
+            .pokemon img 
+            {
+
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div id="Pesquisa">
+            <form>
+
+                <input type="text" placeholder="Digite um Pokémon">
+                <br>
+                <input type="submit" value="buscar">
+
+            </form>
+        </div>
+
+        <?php for($i = 0; $i < 20; $i++):?>            
+            <div id="pokémons">
+
+                <div class="pokemon">
+                    <img src="<?= $pokemons['results'][$i]['sprites']['others']['dream_world']['front_default'] ?>" width="200"> 
+                    <h1><?= $pokemons['results'][$i]['name'] ?></h1> 
+                    <p>peso: 54</p> 
+                    <p>altura: 1,2</p>
+                    <br>                  
+                </div>
+            </div>
+        <?php endfor; ?>
+    </body>
+</html>
